3f583555471d2b6f2995c9e727c3ccd3ed4f12ec
Stephen Leyva <sleyva1297@gmail.com>
2024-06-18 00:14:51 -0700

    Start to add rendering

--- src/ppu/render.rs
+++ src/ppu/render.rs
@@ -1,3 +1,7 @@
+use sdl2::pixels::Palette;
+
+use super::{registers::Control, PPU};
+
 #[rustfmt::skip]
 
 pub static SYSTEM_PALLETE: [(u8,u8,u8); 64] = [
@@ -40,4 +44,100 @@ impl Frame {
             self.data[base + 2] = rgb.2;
         }
     }
+}
+
+pub fn render(ppu: &PPU, frame: &mut Frame) {
+    let bank = if !ppu.ctrl.contains(Control::BACKROUND_PATTERN_ADDR) {
+        0
+    } else {
+        0x1000
+    };
+
+    for i in 0..0x03c0 {
+        // just for now, lets use the first nametable
+        let tile = ppu.vram[i] as u16;
+        let tile_x = i % 32;
+        let tile_y = i / 32;
+        let tile = &ppu.chr_rom[(bank + tile * 16) as usize..=(bank + tile * 16 + 15) as usize];
+
+        for y in 0..=7 {
+            let mut upper = tile[y];
+            let mut lower = tile[y + 8];
+
+            for x in (0..=7).rev() {
+                let value = (1 & upper) << 1 | (1 & lower);
+                upper = upper >> 1;
+                lower = lower >> 1;
+                let rgb = match value {
+                    0 => SYSTEM_PALLETE[0x01],
+                    1 => SYSTEM_PALLETE[0x23],
+                    2 => SYSTEM_PALLETE[0x27],
+                    3 => SYSTEM_PALLETE[0x30],
+                    _ => panic!("can't be"),
+                };
+                frame.set_pixel(tile_x * 8 + x, tile_y * 8 + y, rgb)
+            }
+        }
+    }
+
+    for i in (0..ppu.oam_data.len()).step_by(4).rev() {
+        let tile_idx = ppu.oam_data[i + 1] as u16;
+        let tile_x = ppu.oam_data[i + 3] as usize;
+        let tile_y = ppu.oam_data[i] as usize;
+
+        let flip_vertical = if ppu.oam_data[i + 2] >> 7 & 1 == 1 {
+            true
+        } else {
+            false
+        };
+        let flip_horizontal = if ppu.oam_data[i + 2] >> 6 & 1 == 1 {
+            true
+        } else {
+            false
+        };
+        let pallette_idx = ppu.oam_data[i + 2] & 0b11;
+        let sprite_palette = sprite_palette(ppu, pallette_idx);
+
+        let bank: u16 = if ppu.ctrl.contains(Control::SPRITE_PATTERN_ADDR) {
+            0
+        } else {
+            0x1000
+        };
+
+        let tile =
+            &ppu.chr_rom[(bank + tile_idx * 16) as usize..=(bank + tile_idx * 16 + 15) as usize];
+
+        for y in 0..=7 {
+            let mut upper = tile[y];
+            let mut lower = tile[y + 8];
+            'ololo: for x in (0..=7).rev() {
+                let value = (1 & lower) << 1 | (1 & upper);
+                upper = upper >> 1;
+                lower = lower >> 1;
+                let rgb = match value {
+                    0 => continue 'ololo, // skip coloring the pixel
+                    1 => SYSTEM_PALLETE[sprite_palette[1] as usize],
+                    2 => SYSTEM_PALLETE[sprite_palette[2] as usize],
+                    3 => SYSTEM_PALLETE[sprite_palette[3] as usize],
+                    _ => panic!("can't be"),
+                };
+                match (flip_horizontal, flip_vertical) {
+                    (false, false) => frame.set_pixel(tile_x + x, tile_y + y, rgb),
+                    (true, false) => frame.set_pixel(tile_x + 7 - x, tile_y + y, rgb),
+                    (false, true) => frame.set_pixel(tile_x + x, tile_y + 7 - y, rgb),
+                    (true, true) => frame.set_pixel(tile_x + 7 - x, tile_y + 7 - y, rgb),
+                }
+            }
+        }
+    }
+}
+
+fn sprite_palette(ppu: &PPU, pallete_idx: u8) -> [u8; 4] {
+    let start = 0x11 + (pallete_idx * 4) as usize;
+    [
+        0,
+        ppu.palette_table[start],
+        ppu.palette_table[start + 1],
+        ppu.palette_table[start + 2],
+    ]
 }
